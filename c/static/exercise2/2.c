aec259707109a4072ab5251db12e0c3060ba9539
racterub <racterub@gmail.com>
2020-03-20 11:34:37 +0800

    Meow

--- c/static/exercise2/2.c
+++ c/static/exercise2/2.c
@@ -5,30 +5,34 @@ int main(int argc, char *argv[])
     int pal = 0;
     int data;
     int tmp;
-    int counter = 0;
-    printf("Input: ");
-    scanf("%d", &data);
+    int counter;
+    while (1) {
+        counter = 0;
+        pal = 0;
+        printf("Input: ");
+        scanf("%d", &data);
     
-    //exception
-    if (data < 0) return 0; 
+        //exception
+        if (data < 0) break; 
 
-    tmp = data;
-    while (tmp > 0) {
-        counter++;
-        pal = pal * 10 + tmp % 10;
-        tmp /= 10;
-    }
-    
-    //exception
-    if (counter != 5) {
-        puts("Not a 5-digit number");
-    }
+        tmp = data;
+        while (tmp > 0) {
+            counter++;
+            pal = pal * 10 + tmp % 10;
+            tmp /= 10;
+        }
+        
+        //exception
+        if (counter != 5) {
+            puts("Not a 5-digit number");
+            break;
+        }
 
-    if (pal == data) {
-        puts("True");
-    } else {
-        puts("False");
+        if (pal == data) {
+            puts("True");
+        } else {
+            puts("False");
+        }
     }
-
     return 0;
 }
