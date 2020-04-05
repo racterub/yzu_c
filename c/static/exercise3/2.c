9f313b9f8ee1556978d662a439de05690ce2aa9c
racterub <racterub@gmail.com>
2020-04-05 23:28:43 +0800

    Fixes

--- c/static/exercise3/2.c
+++ c/static/exercise3/2.c
@@ -2,23 +2,25 @@
 #include <math.h>
 
 // define out loop wil run 100 times
-#define LOOP 10
+#define LOOP 30
 
 int main(int argc, char *argv[])
 {
     int data;
-    int mod;
+    unsigned long long mod;
+    //double mod;
     float answer = 1.0;
     printf("Input: ");
     scanf("%d", &data);
-    for (int i=0;i<LOOP;i++) {
+    for (int i=1;i<=LOOP;i++) {
         mod = 1; 
         for (int j=1;j<=i;j++) {
             mod *= j;
         }
-        printf("%.3f\n", answer);
-        answer += pow(data, i) / mod;
+        printf("%.10f\n", answer);
+        printf("%llu\n", mod);
+        answer += pow(data, i) / (float)mod;
     }
-    printf("==> %.3f\n", answer);
+    printf("==> %.10f\n", answer);
     return 0;   
 }
