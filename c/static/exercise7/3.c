61d34b59fbdcbb379e8d0a4a914c7323bad12781
racterub <vivi.450@hotmail.com>
2020-04-24 16:18:12 +0800

    fix exercise7/3

--- c/static/exercise7/3.c
+++ c/static/exercise7/3.c
@@ -2,21 +2,24 @@
 #include <stdlib.h>
 
 void printPascal(int n) {
-    int arr[n];
-    int result = 1;
+    // see method 3 at [HERE](https://www.geeksforgeeks.org/pascal-triangle/)
+    int result;
     for (int i=1;i<=n;i++) {
-        arr[i-1] = result;
-        result = result * (n - i) / i;
-    }
-    for (int i=0;i<n;i++) {
-        printf("%d ", arr[i]);
+        result = 1;
+        for (int j=1;j<=i;j++) {
+            printf("%d ", result);
+            result = result * (i - j) / j;
+        }
+        puts("");
     }
 }
 
 
 int main(int argc, char *argv[])
 {
-
-    printPascal(5);
+    int input;
+    printf("Height of Pascal triangle: ");
+    scanf("%d", &input);
+    printPascal(input);
     return 0;
 }
