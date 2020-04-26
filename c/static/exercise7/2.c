5fbdebd860575516dda7f4db3842385f26046020
racterub <vivi.450@hotmail.com>
2020-04-26 21:12:35 +0800

    fix exercise7/2

--- c/static/exercise7/2.c
+++ c/static/exercise7/2.c
@@ -2,14 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
+int checkDup(int arr[],int arrSize, int search) {
+    for (int i=0;i<arrSize;i++) {
+        if (search == arr[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
-    int arr[6];
-    
+    int arr[6] = {0};
+    int tmp; 
     srand(time(NULL));
-    
-    for (int i=0;i<6;i++) {
-        arr[i] = rand() % 49 + 1;
+    int i = 0;
+    while (i < 6) {
+        tmp = 1 + rand() % 49;
+        if (checkDup(arr, 6, tmp) == 1) {
+           continue; 
+        }
+        arr[i] = tmp;
+        i++;
     }
     for (int i=0;i<6;i++) {
         printf("%d\n", arr[i]);
